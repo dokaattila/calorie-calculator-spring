d3ca5ea6fe2269cc28f7f6ab096fb1eea340f5a4
dokaattila <dokaatti@gmail.com>
2022-04-17 20:49:43 +0200

    add model class

--- src/main/java/hu/da/ccs/model/Meal.java
+++ src/main/java/hu/da/ccs/model/Meal.java
@@ -1,4 +1,18 @@
 package hu.da.ccs.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
+
+@AllArgsConstructor
+@Jacksonized
+@Data
+@Getter
 public class Meal {
+
+    private int id;
+    private String name;
+    private double amount;
+
 }
